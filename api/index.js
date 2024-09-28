8e292345a992b903e85fb3f941b6b23374cbae3c
Mansi Mittal <mansi27mittal@gmail.com>
2024-09-29 01:07:45 +0530

    port

--- api/index.js
+++ api/index.js
@@ -162,9 +162,12 @@ app.get('/post/:id', async (req, res) => {
   res.json(postDoc);
 })
 
-app.listen(4000, () => {
-  console.log('Server is running on port 4000');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
 
 
 
